4efcc3a989b602881176e20f8bdb1ec34f41853d
albert <albert.suarez.molgo@est.fib.upc.edu>
2016-07-06 12:11:38 +0200

    initialize search view

--- app/src/main/java/pereberge/sumproject/activity/ReservationActivity.java
+++ app/src/main/java/pereberge/sumproject/activity/ReservationActivity.java
@@ -1,15 +1,24 @@
 package pereberge.sumproject.activity;
 
+import android.annotation.TargetApi;
+import android.app.SearchManager;
+import android.content.Context;
+import android.database.MatrixCursor;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
 import java.util.Date;
+import java.util.List;
+
 import pereberge.sumproject.R;
+import pereberge.sumproject.adapter.SearchAdapter;
 import pereberge.sumproject.domain.Reservation;
 import pereberge.sumproject.services.ReservationService;
 import pereberge.sumproject.utils.DateUtils;
@@ -19,9 +28,10 @@ public class ReservationActivity extends AppCompatActivity {
 
     public static final String INTENT_RESERVATION = "DATE";
 
-    private EditText editTextName;
     private String name;
     private ReservationService reservationService;
+
+    private List<String> items;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,12 +42,29 @@ public class ReservationActivity extends AppCompatActivity {
         toolbar.setTitle("Reserva");
         setSupportActionBar(toolbar);
 
+        reservationService = ServiceFactory.getReservationService(getApplicationContext());
+        this.items = reservationService.getPartnerNames();
+
+        SearchManager manager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView search = (SearchView) findViewById(R.id.newName);
+        assert search != null;
+        search.setSearchableInfo(manager.getSearchableInfo(getComponentName()));
+        search.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String query) {
+                loadHistory(query);
+                return true;
+            }
+        });
+
         TextView day = (TextView) findViewById(R.id.day);
         TextView hour = (TextView) findViewById(R.id.timeZone);
-        editTextName = (EditText) findViewById(R.id.newName);
         Button book = (Button) findViewById(R.id.botoReserva);
-
-        reservationService = ServiceFactory.getReservationService(getApplicationContext());
 
         final Date date = DateUtils.millisToDate(getIntent().getLongExtra(INTENT_RESERVATION, 0));
 
@@ -50,7 +77,7 @@ public class ReservationActivity extends AppCompatActivity {
         book.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                name = editTextName.getText().toString();
+                name = "TEST";
                 if (!name.isEmpty()) {
                     Reservation reservation = new Reservation(name, date);
                     reservationService.save(reservation);
@@ -59,5 +86,27 @@ public class ReservationActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    private void loadHistory(String query) {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            String[] columns = new String[] { "_id", "text" };
+            Object[] temp = new Object[] { 0, "default" };
+
+            MatrixCursor cursor = new MatrixCursor(columns);
+
+            for (int i = 0; i < items.size(); i++) {
+                temp[0] = i;
+                temp[1] = items.get(i);
+                cursor.addRow(temp);
+            }
+
+            final SearchView search = (SearchView) findViewById(R.id.newName);
+            assert search != null;
+            search.setSuggestionsAdapter(new SearchAdapter(this, cursor, items));
+        }
+
     }
 }
